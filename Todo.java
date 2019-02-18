8ace09b525f36fd4d0dda8f00e6045196a28d44f
Kiley Peters <kiley.m.peters-1@ou.edu>
2019-02-18 09:36:44 -0600

    Updated todo

--- Todo.java
+++ Todo.java
@@ -1,18 +1,28 @@
 public class Todo {/*
-Lab Section_________: XX
-Last Modified_______: 20XX-XX-XX
-OU ID_______________: XXXXXXXXX
-OU EMAIL____________: XXX@ou.edu
+Lab Section_________: 11
+Last Modified_______: 2019-02-14
+OU ID_______________: 113371387
+OU EMAIL____________: Kiley.m.peters-1@ou.edu
 
 Tasks:
-1. XXX
-2. XXX
+1. Complete calculateTwoTokens in Calculator class
+2. Complete calculateThreeTokens in Calculator class
+3. Complete execute in Calculator class
+4. Complete parseAndExecute in Caclulator class
+5. Complete test cases.
+
 
 Estimated Time (minutes):
-1. XXX
-2. XXx
+1. 60
+2. 60
+3. 60
+4. 60
+5. 300
 
 Actual Time (minutes):
-1. XXX
-2. XXX
+1. 0
+2. 0
+3. 0
+4. 0
+5. 0
 */}
